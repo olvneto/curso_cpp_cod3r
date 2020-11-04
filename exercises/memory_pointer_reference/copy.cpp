09d89e2924078219adca05b2afbdb239966c9a5f
olvneto <olvneto@yahoo.com.br>
2020-11-03 22:57:35 -0300

    Estudo V4

---
+++ exercises/memory_pointer_reference/copy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 2;
+    int b = a;
+
+    cout << a << " " << b << endl;
+
+    a++;
+    b--;
+
+    cout << a << " " << b << endl;
+    return 0;
+}
